b5b5dbde98c0040ce12310b3e8a7bf3c7df2e8db
Mohammad <mohammad.jahangirii79@gmail.com>
2023-02-01 17:12:26 +0330

    wip

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        echo "index method in HomeController";
+    }
+
+}
