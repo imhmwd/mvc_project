8e73e386569344dfd3537f85f07fda426ba0ff6a
Mohammad <mohammad.jahangirii79@gmail.com>
2023-03-06 17:50:17 +0330

    wip

--- system/Auth/Auth.php
+++ system/Auth/Auth.php
@@ -8,71 +8,63 @@ use System\Session\Session;
 
 class Auth
 {
-   
+
     private $redirectTo = "/login";
 
     private function userMethod()
     {
-      if(!Session::get('user'))
-      {
-          return redirect($this->redirectTo);
-      }
-      $user = User::find(Session::get('user'));
-      if(empty($user))
-      {
-          Session::remove('user');
-          return redirect($this->redirectTo);
-      }
-      else
-      return $user;
+        if (!Session::get('user')) {
+            return redirect($this->redirectTo);
+        }
+        $user = User::find(Session::get('user'));
+        if (empty($user)) {
+            Session::remove('user');
+            return redirect($this->redirectTo);
+        } else {
+            return $user;
+        }
     }
 
     private function checkMethod()
     {
-        if(!Session::get('user'))
-        {
+        if (!Session::get('user')) {
             return redirect($this->redirectTo);
         }
+
         $user = User::find(Session::get('user'));
-        if(empty($user))
-        {
+
+        if (empty($user)) {
             Session::remove('user');
             return redirect($this->redirectTo);
+        } else {
+            return true;
         }
-        else
-        return true;
     }
 
     private function checkLoginMethod()
     {
-        if(!Session::get('user'))
-        {
+        if (!Session::get('user')) {
             return false;
         }
         $user = User::find(Session::get('user'));
-        if(empty($user))
-        {
-           return false;
+        if (empty($user)) {
+            return false;
+        } else {
+            return true;
         }
-        else
-        return true;
     }
 
     private function loginByEmailMethod($email, $password)
     {
         $user = User::where('email', $email)->get();
-        if(empty($user))
-        {
+        if (empty($user)) {
             error('login', 'کاربر وجود ندارد');
             return false;
         }
-        if(password_verify($password, $user[0]->password) && $user[0]->is_active == 1)
-        {
+        if (password_verify($password, $user[0]->password) && $user[0]->is_active == 1) {
             Session::set("user", $user[0]->id);
             return true;
-        }
-        else
-        {
+        } else {
             error("login", 'کلمه ی عبور اشتباه است');
             return false;
         }
@@ -81,13 +73,10 @@ class Auth
     private function loginByIdMethod($id)
     {
         $user = User::find($id);
-        if(empty($user))
-        {
+        if (empty($user)) {
             error("login", "کاربر وجود ندارد");
             return false;
-        }
-        else
-        {
+        } else {
             Session::set("user", $user->id);
             return true;
         }
@@ -112,7 +101,7 @@ class Auth
     private function methodCaller($method, $arguments)
     {
         $suffix = 'Method';
-        $methodName = $method.$suffix;
+        $methodName = $method . $suffix;
         return call_user_func_array(array($this, $methodName), $arguments);
     }
 }
