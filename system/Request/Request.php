fd403dac1f715b9bd2c27f10dc0d13c9343a5f97
Mohammad <mohammad.jahangirii79@gmail.com>
2023-03-06 17:40:45 +0330

    wip

--- system/Request/Request.php
+++ system/Request/Request.php
@@ -2,13 +2,14 @@
 
 namespace System\Request;
 
+
 use System\Request\Traits\HasFileValidationRules;
 use System\Request\Traits\HasRunValidation;
 use System\Request\Traits\HasValidationRules;
 
 class Request
 {
-    use HasFileValidationRules, HasRunValidation, HasValidationRules;
+    use HasValidationRules, HasFileValidationRules, HasRunValidation;
 
     protected $errorExist = false;
     protected $request;
@@ -16,50 +17,41 @@ class Request
     protected $errorVariablesName = [];
 
     public function __construct()
-    {  
+    {
         if (isset($_POST)) {
             $this->postAttributes();
         }
-
-        if (!empty($_FILES)) {
+        if (!empty($_FILES))
             $this->files = $_FILES;
-        }
         $rules = $this->rules();
-
-        if (!empty($rules)){
-            $this->run($rules);
-        }
-
+        empty($rules) ?: $this->run($rules);
         $this->errorRedirect();
     }
 
-    protected function run($roles)
-    {
-        foreach ($roles as $attribute => $values) {
-            $ruleArray = explode('|', $values);
-
-            if (in_array('file', $ruleArray)) {
-                unset($ruleArray[array_search('file', $ruleArray)]);
-                $this->fileValidation($attribute, $ruleArray);
-
-            } elseif (in_array('number', $ruleArray)) {
-                $this->numberValidation($attribute, $ruleArray);
-            } else {
-                $this->normalValidation($attribute, $ruleArray);
-
-            }
-
-        }
-    }
 
     protected function rules()
     {
         return [];
     }
 
+    protected function run($rules)
+    {
+        foreach ($rules as $att => $values) {
+            $ruleArray = explode('|', $values);
+            if (in_array('file', $ruleArray)) {
+                unset($ruleArray[array_search('file', $ruleArray)]);
+                $this->fileValidation($att, $ruleArray);
+            } elseif (in_array('number', $ruleArray)) {
+                $this->numberValidation($att, $ruleArray);
+            } else {
+                $this->normalValidation($att, $ruleArray);
+            }
+        }
+    }
+
     public function file($name)
     {
-        return $this->file[$name] ?? false;
+        return isset($this->files[$name]) ? $this->files[$name] : false;
     }
 
     protected function postAttributes()
@@ -74,5 +66,6 @@ class Request
     {
         return $this->request;
     }
+
 
 }
--- system/Request/Traits/HasValidationRules.php
+++ system/Request/Traits/HasValidationRules.php
@@ -11,74 +11,59 @@ trait HasValidationRules
     {
         foreach ($ruleArray as $rule) {
             if ($rule == 'required') {
-
                 $this->required($name);
-
             } elseif (strpos($rule, "max:") === 0) {
-
-                $rule = str_replace('max:', '', $rule);
+                $rule = str_replace('max:', "", $rule);
                 $this->maxStr($name, $rule);
-
             } elseif (strpos($rule, "min:") === 0) {
-
-                $rule = str_replace('min:', '', $rule);
+                $rule = str_replace('min:', "", $rule);
                 $this->minStr($name, $rule);
-
             } elseif (strpos($rule, "exists:") === 0) {
-
-                $rule = str_replace('exists:', '', $rule);
+                $rule = str_replace('exists:', "", $rule);
                 $rule = explode(',', $rule);
-                $key = !isset($rule[1]) ? null : $rule[1];
-                $this->exsitsIn($name, $rule[0], $key);
-
+                $key = isset($rule[1]) == false ? null : $rule[1];
+                $this->existsIn($name, $rule[0], $key);
             } elseif ($rule == 'email') {
-
                 $this->email($name);
-
             } elseif ($rule == 'date') {
-
                 $this->date($name);
-
             }
         }
-
     }
 
-    public function numberValidation($name, $ruleArray)
+        public function numberValidation($name, $ruleArray)
     {
-        foreach ($ruleArray as $rule) {
-            if ($rule == 'required') {
-
-                $this->required($name);
-
-            } elseif (strpos($rule, "max:") === 0) {
-
-                $rule = str_replace('max:', '', $rule);
+        foreach($ruleArray as $rule){
+            if($rule == 'required')
+            $this->required($name);
+            elseif(strpos($rule, "max:") === 0)
+            {
+                $rule = str_replace('max:', "", $rule);
                 $this->maxNumber($name, $rule);
-
-            } elseif (strpos($rule, "min:") === 0) {
-
-                $rule = str_replace('min:', '', $rule);
+            }
+            elseif(strpos($rule, "min:") === 0)
+            {
+                $rule = str_replace('min:', "", $rule);
                 $this->minNumber($name, $rule);
-
-            } elseif (strpos($rule, "exists:") === 0) {
-
-                $rule = str_replace('exists:', '', $rule);
+            }
+            elseif(strpos($rule, "exists:") === 0)
+            {
+                $rule = str_replace('exists:', "", $rule);
                 $rule = explode(',', $rule);
-                $key = !isset($rule[1]) ? null : $rule[1];
-                $this->exsitsIn($name, $rule[0], $key);
-
-            } elseif ($rule == 'number') {
+                $key = isset($rule[1]) == false ? null : $rule[1];
+                $this->existsIn($name, $rule[0], $key);
+            }
+            elseif($rule == 'number')
+            {
                 $this->number($name);
             }
         }
-
     }
 
     protected function maxStr($name, $count)
     {
-        if ($this->checkFieldExist($name)) {
-            if (strlen($this->request[$name]) > $count && $this->checkFirstError($name)) {
+        if($this->checkFieldExist($name)){
+            if (strlen($this->request[$name]) >= $count && $this->checkFirstError($name)){
                 $this->setError($name, "max length equal or lower than $count character");
             }
         }
@@ -86,8 +71,8 @@ trait HasValidationRules
 
     protected function minStr($name, $count)
     {
-        if ($this->checkFieldExist($name)) {
-            if (strlen($this->request[$name]) < $count && $this->checkFirstError($name)) {
+        if($this->checkFieldExist($name)){
+            if (strlen($this->request[$name]) <= $count && $this->checkFirstError($name)){
                 $this->setError($name, "min length equal or upper than $count character");
             }
         }
@@ -95,8 +80,8 @@ trait HasValidationRules
 
     protected function maxNumber($name, $count)
     {
-        if ($this->checkFieldExist($name)) {
-            if ($this->request[$name] > $count && $this->checkFirstError($name)) {
+        if($this->checkFieldExist($name)){
+            if ($this->request[$name] >= $count && $this->checkFirstError($name)){
                 $this->setError($name, "max number equal or lower than $count character");
             }
         }
@@ -104,8 +89,8 @@ trait HasValidationRules
 
     protected function minNumber($name, $count)
     {
-        if ($this->checkFieldExist($name)) {
-            if ($this->request[$name] < $count && $this->checkFirstError($name)) {
+        if($this->checkFieldExist($name)){
+            if ($this->request[$name] <= $count && $this->checkFirstError($name)){
                 $this->setError($name, "min number equal or upper than $count character");
             }
         }
@@ -113,53 +98,55 @@ trait HasValidationRules
 
     protected function required($name)
     {
-        if ((!isset($this->request[$name]) || $this->request[$name] === '') &&
-            $this->checkFirstError($name)) {
-            $this->setError($name, "$name is required");
+        if((!isset($this->request[$name]) || $this->request[$name] === '') && $this->checkFirstError($name)){
+            $this->setError($name,"$name is required");
         }
     }
 
     protected function number($name)
     {
-        if ($this->checkFieldExist($name)) {
-            if (!is_numeric($this->request[$name]) && $this->checkFirstError($name)) {
-                $this->setError($name, "$name must be number format");
+        if($this->checkFieldExist($name)){
+            if(!is_numeric($this->request[$name]) && $this->checkFirstError($name))
+            {
+                $this->setError($name,"$name must be number format");
             }
         }
     }
 
     protected function date($name)
     {
-        if ($this->checkFieldExist($name)) {
-            if (!preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $this->request[$name]) && $this->checkFirstError($name)) {
-                $this->setError($name, "$name must be date format(Y-m-d)");
-            }
+        if($this->checkFieldExist($name)){
+           if(!preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$this->request[$name]) && $this->checkFirstError($name)){
+            $this->setError($name,"$name must be date format");
+           }
         }
     }
 
     protected function email($name)
     {
-        if ($this->checkFieldExist($name)) {
-            if (!filter_var($this->request[$name], FILTER_VALIDATE_EMAIL) && $this->checkFirstError($name)) {
-                $this->setError($name, "$name must be email format");
+        if($this->checkFieldExist($name)){
+            if(!filter_var($this->request[$name], FILTER_VALIDATE_EMAIL) && $this->checkFirstError($name))
+            {
+                $this->setError($name,"$name must be email format");
             }
         }
     }
 
-    public function existsIn($name, $table, $field = 'id  ')
+    public function existsIn($name, $table, $field = "id")
     {
-        if ($this->checkFieldExist($name)) {
-            if ($this->checkFirstError($name)) {
+        if($this->checkFieldExist($name)){
+            if($this->checkFirstError($name)){
                 $value = $this->$name;
-                $sql = "SELECT COUNT(*) FROM $table $field = ? ";
+                $sql = "SELECT COUNT(*) FROM $table WHERE $field = ?";
                 $statement = DBConnection::getDBConnectionInstance()->prepare($sql);
                 $statement->execute([$value]);
                 $result = $statement->fetchColumn();
-                if ($result == 0) {
-                    $this->setError($name, "$name not already exist");
+                if($result == 0 || $result === false){
+                    $this->setError($name,"$name not already exist");
                 }
             }
         }
     }
+
 
 }
